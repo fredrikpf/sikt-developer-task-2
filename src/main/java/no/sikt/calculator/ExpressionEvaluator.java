eaf78566b0f9c94d64fab05670e7aa39b339066a
Fredrik Pfeil <57641333+fredrikpf@users.noreply.github.com>
2022-06-14 21:45:53 +0200

    evaluate expressions according to given testvalues

--- src/main/java/no/sikt/calculator/ExpressionEvaluator.java
+++ src/main/java/no/sikt/calculator/ExpressionEvaluator.java
@@ -1,13 +1,85 @@
 package no.sikt.calculator;
 
+import java.util.Stack;
+
 import static java.util.Objects.nonNull;
 
 public class ExpressionEvaluator {
+    private final transient String expression;
+    private final transient Stack<Integer> numberStack = new Stack<>();
+
+    /**
+     * Checks if expression is valid for computation.
+     * @param expression The expression that should be checked.
+     */
     public ExpressionEvaluator(String expression) {
         assert nonNull(expression);
+        String[] expArray = expression.split(" ");
+        assert isNumber(expArray[0]);
+        assert oneMoreOperandsThanOperators(expArray);
+        this.expression = expression;
     }
 
+    /**
+     * Calculates the expression.
+     * @return The result of expression
+     */
     public Integer getResult() {
-        return 2;
+        String[] e = expression.split(" ");
+
+        for (String current : e) {
+            if (isNumber(current)) {
+                Integer d = Integer.parseInt(current);
+                numberStack.push(d);
+            } else if (isOperation(current)) {
+                Integer b = numberStack.pop();
+                Integer a = numberStack.pop();
+                Integer result = calculate(a, b, current);
+                numberStack.push(result);
+            }
+        }
+        return numberStack.pop();
     }
+
+
+    private Boolean isNumber(String a) {
+        return a.matches("^[0-9]+$");
+    }
+
+    private Boolean isOperation(String a) {
+        return a.matches("^[\\Q+-*/\\E]$");
+    }
+
+    private Integer calculate(Integer a, Integer b, String operator) {
+        switch (operator) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                throw new IllegalArgumentException(operator + " is not an operator.");
+        }
+    }
+
+    private Boolean oneMoreOperandsThanOperators(String... exp) {
+        int amountOfOperands = 0;
+        int amountOfOperators = 0;
+
+        for (String str : exp) {
+            if (isNumber(str)) {
+                amountOfOperands++;
+            } else if (isOperation(str)) {
+                amountOfOperators++;
+            } else {
+                throw new IllegalArgumentException(str + " is neither a number nor an operator.");
+            }
+        }
+
+        return amountOfOperands - amountOfOperators == 1;
+    }
+
 }
--- src/test/java/no/sikt/calculator/ExpressionEvaluatorTest.java
+++ src/test/java/no/sikt/calculator/ExpressionEvaluatorTest.java
@@ -2,15 +2,76 @@ package no.sikt.calculator;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ExpressionEvaluatorTest {
 
     @Test
-    void shouldReturnValueWhenExpressionIsValid()  {
+    void shouldReturnValueWhenExpressionIsValid() {
         var expected = 2;
         var expression = "1 1 +";
         var actual = new ExpressionEvaluator(expression).getResult();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testAllCases() {
+        try {
+            File myObj = new File("src/test/resources/examples.tsv");
+            Scanner myReader = new Scanner(myObj);
+            myReader.nextLine(); //Skip header
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                var singleCase = data.split("\t");
+                checkSingleCase(singleCase[0], Integer.parseInt(singleCase[1]));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    private void checkSingleCase(String expression, Integer expected) {
+        var actual = new ExpressionEvaluator(expression).getResult();
+        assertEquals(expected, actual, "Expression: " + expression);
+    }
+
+
+    @Test
+    void mathematicallyInvalidExpressionException() {
+        String invalidExpression = "1 0 /";
+        assertThrows(
+                ArithmeticException.class,
+                () -> new ExpressionEvaluator(invalidExpression).getResult(),
+                invalidExpression + " didn't throw an error!");
+    }
+
+    @Test
+    void invalidExpressionThrowsException() {
+        String[] invalidExpressions = {
+            null,
+            "+ 1 2 4",
+            "d f g 12",
+            "1 2 3 4 5 6",
+            "1 + + + +"
+        };
+
+        for (String invalidExp : invalidExpressions) {
+            invalidExpressionThrowsAssertionError(invalidExp);
+        }
+    }
+
+    private void invalidExpressionThrowsAssertionError(String invalidExpression) {
+        assertThrows(
+                AssertionError.class,
+                () -> new ExpressionEvaluator(invalidExpression).getResult(),
+                invalidExpression + " didn't throw an error!");
     }
 }
